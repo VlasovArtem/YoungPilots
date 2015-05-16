8b0fabbd25f7e9d29165671e262b7cf1d8430be5
Vlasov Artem <vlasovartem21@gmail.com>
2015-05-16 19:10:25 +0300

    Update angularjs modules, change structure of the index.html

--- app/conferences-page/ConferencesServices.js
+++
@@ -1,1 +0,0 @@
-var services=angular.module('conference.services',['ngResource']);services.factory('ConferenceCityCoordinates',['$q',function($q){var factory={getCoordinates:function(city){var deferred=$q.defer();deferred.resolve(LatLngFactory.get({"address":scope.city}));return deferred;}};return factory;}]);
